02b585fc8b47105e1c28036cf00ff3f1beb9b97b
AlexandrKozlovskiy <k.sasha1994@>
2019-09-25 19:50:17 +0300

    added autimatic replacement of empty lines

--- sty/axessibility.lua
+++ sty/axessibility.lua
@@ -3,18 +3,15 @@ pdf.setminorversion(0)
 
 local open_dls = false
 local open_double_dls = false
-local insert_space_in_empty_strings_if_in_formula = false
-
 -- we use this function to switch on or switch off automatic replacement of $$ and $ 
 -- a - the flag (true/false) specifying if the package should do the replacements 
 -- b - replace empty lines in formula on some value or not
-function replace_dls_and_double_dls(a,b) 
+function replace_dls_and_double_dls(a) 
 	if a and not luatexbase.in_callback("process_input_buffer", "process_input_buffer") then
 		luatexbase.add_to_callback("process_input_buffer", replace_chars_callback, "process_input_buffer")
 	elseif not a and luatexbase.in_callback("process_input_buffer", "process_input_buffer") then
 		luatexbase.remove_from_callback("process_input_buffer", "process_input_buffer")
 	end
-	insert_space_in_empty_strings_if_in_formula = b
 end
 
 -- the callback that performs the replacement
@@ -32,8 +29,8 @@ function replace_chars_callback(a)
 			return group1..group2
 		end)
 		
-		-- if the user wants to automatically replace empty lines in the formula, we must replace them by some value, eg % character
-		if(open_double_dls or open_dls) and insert_space_in_empty_strings_if_in_formula and a == "" then
+		-- We must replace empty lines by some value, eg % character
+		if(open_double_dls or open_dls) and a == "" then
 			return "%"
 		end
 
@@ -68,14 +65,5 @@ function replace_chars_callback(a)
 			texio.write_nl("the line after replacement is: "..a)
 		end]]
 		return a
-	end
-end
--- check if we open some paragraph or not
-local in_par=false
- 
--- open and close paragraph tags
-function checkpar() 
-	if in_par then
-		tex.print("\\tagmcend\\tagstructend")
 	end
 end
